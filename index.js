bce4dbcfb96e6e5afc46008b33d08b00d5ed4997
RiotonZong <a4214679@pccu.edu.tw>
2018-08-03 17:42:28 +0800

    0803_1742

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 module.exports = (api, opts, rootOpts) => {
-  const isProd = process.env.NODE_ENV === 'production';
-  const multiPageConfig = opts.pages;
+    const isProd = process.env.NODE_ENV === 'production';
+    const multiPageConfig = opts.pages;
 
-  api.chainWebpack(config => {
-    if(isProd) {
-      if(!multiPageConfig) {
-        config
-          .plugin('html')
-          .tap(args => {
-            args[0].filename = api.resolve('templates/client/index.html');
-            return args;
-          });
-      } else {
-        const pages = Object.keys(multiPageConfig);
-        pages.forEach(name => {
-          config
-            .plugin(`html-${name}`)
-            .tap(args => {
-              args[0].filename = api.resolve(`templates/client/${name}.html`);
-              return args;
-            });
-        })
-      }
-    }
-  })
+    api.chainWebpack(config => {
+        if (isProd) {
+            if (!multiPageConfig) {
+                config
+                    .plugin('html')
+                    .tap(args => {
+                        args[0].filename = api.resolve('templates/index.html');
+                        return args;
+                    });
+            } else {
+                const pages = Object.keys(multiPageConfig);
+                pages.forEach(name => {
+                    config
+                        .plugin(`html-${name}`)
+                        .tap(args => {
+                            args[0].filename = api.resolve(`templates/${name}.html`);
+                            return args;
+                        });
+                })
+            }
+        }
+    })
 }
